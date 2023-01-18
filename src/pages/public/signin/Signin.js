2cca62d7f0b4bb8678574d87147b5ba265379e59
DESKTOP-J1T8E70\davs <Davit.sahakyan@volo.global>
2023-01-18 20:46:40 +0400

    fix validation

--- src/pages/public/signin/Signin.js
+++ src/pages/public/signin/Signin.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React, {useState}  from 'react';
 import {Link} from "react-router-dom";
 import { NavLink, useNavigate } from 'react-router-dom';
 
@@ -6,23 +6,34 @@ import { signInWithEmailAndPassword } from 'firebase/auth';
 import { firestore } from "../../../firebase";
 import { auth } from '../../../firebase';
 import { addDoc, collection } from "@firebase/firestore";
-import { Formik } from 'formik';
+import { Formik, Form, Field, ErrorMessage  } from 'formik';
+import { EyeOutlined, EyeInvisibleOutlined } from '@ant-design/icons';
 
 import logo from "../../../assets/images/logo.png";
 import './Signin.css';
 
+
 const Login = () => {
     const navigate = useNavigate();
     const ref = collection(firestore, "users");
+    const [showHide, setShowHide] = useState(false);
+
+    // Synchronous validation function
+    const validate = value => {
+        let errorMessage;
+        if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+          errorMessage = 'Invalid email address';
+        }
+        return errorMessage;
+      };
 
 
 return ( <div className="loginPage">
-            <div className='picture'>
-            </div>
+            <div className='picture'></div>
             <div className='loginContainer'>
                 <div className='loginContent'>
                     <div className='logoContainer'>
-                        <img src={logo} alt="logo"></img>
+                        <img src={logo} alt="logo"/>
                     </div>
                     <Formik
                         initialValues={{ email: '', password: '' }}
@@ -35,6 +46,15 @@ return ( <div className="loginPage">
                             ) {
                                 errors.email = 'Invalid email address';
                             }
+                            
+                            if (!values.password) {
+                                errors.password = 'Required field';
+                            } else if (
+                                !/^(?=.*\d)(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/i.test(values.password)
+                            ) {
+                                errors.password = 'Invalid password';
+                            }
+
                             return errors;
                         }}
                         onSubmit={async (values, { setSubmitting }) => {
@@ -61,29 +81,39 @@ return ( <div className="loginPage">
                             handleSubmit,
                             isSubmitting,
                         }) => (
-                            <form onSubmit={handleSubmit} className="loginForm">
+                            <Form onSubmit={handleSubmit} className="loginForm">
+                                
+                                <div className='formFieldContainer'>
+                                    <label>Login</label>
+                                    <Field 
+                                        name="email"
+                                        validate={validate}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.email}
+                                        className={`inputStyle ${ errors.email && touched.email && errors.email ? 'inputError' : '' }`}
+                                    />
+                                    <span className='errorMessasge'>{errors.email && touched.email && errors.email}</span>
+                                </div>
 
-                                <label>Login</label>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    value={values.email}
-                                    className="inputStyle"
-                                />
-                                {errors.email && touched.email && errors.email}
+                                <div className='formFieldContainer'>
+                                    <label>Password</label>
+                                    <div className={`inputStyle ${ errors.password && touched.password && errors.password ? 'inputError' : '' }`} id="passwordDiv">
+                                        <Field
+                                            type={showHide ? "text" : "password"}
+                                            name="password"
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.password}
+                                            className="passworsInput"
+                                        />
+                                        <div className="showHidebtn" onClick={(e) => { setShowHide(!showHide) }}>
+                                            {showHide ? <EyeOutlined /> : <EyeInvisibleOutlined />}
+                                        </div>
+                                    </div>
+                                    <span className='errorMessasge'>{errors.password && touched.password && errors.password}</span>    
+                                </div>
 
-                                <label>Password</label>
-                                <input
-                                    type="password"
-                                    name="password"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    value={values.password}
-                                    className="inputStyle"
-                                />
-                                {errors.password && touched.password && errors.password}
 
                                 <button type="submit" disabled={isSubmitting} className="loginSubmit">
                                     Login
@@ -92,7 +122,7 @@ return ( <div className="loginPage">
                                 <div>
                                     <span>Not a member? <Link to={'/signup'}>signup</Link></span>
                                 </div>
-                            </form>
+                            </Form>
                         )}
                     </Formik>
                 </div>
--- src/pages/public/signup/Signup.jsx
+++ src/pages/public/signup/Signup.jsx
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React, {useState}  from 'react';
 import {Link} from "react-router-dom";
 import { NavLink, useNavigate } from 'react-router-dom';
 
@@ -6,36 +6,48 @@ import {  createUserWithEmailAndPassword  } from 'firebase/auth';
 import { auth } from '../../../firebase';
 import { firestore } from "../../../firebase";
 import { addDoc, collection } from "@firebase/firestore";
-import { Formik } from 'formik';
+import { Formik, Form, Field } from 'formik';
 
 import logo from "../../../assets/images/logo.png";
 import './Signup.css';
+import { EyeOutlined, EyeInvisibleOutlined } from '@ant-design/icons';
 
 const Login = () => {
     const ref = collection(firestore, "users");
     const navigate = useNavigate();
+    const [showHidePass, setShowHidePass] = useState(false);
+    const [showHideConfirmPass, setShowHideConfirmPass] = useState(false);
 
+    // Synchronous validation function
+    const validate = value => {
+        let errorMessage;
+        if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+            errorMessage = 'Invalid email address';
+        }
+        return errorMessage;
+        };
+
+    const passValidate = value => {
+        let errorMessage;
+        if (value) {
+            errorMessage = 'Required field';
+        } else if (
+            !/^(?=.*\d)(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/i.test(value)
+        ) {
+            errorMessage = 'Invalid password';
+        }
+        return errorMessage
+    }
 
 return ( <div className="loginPage">
             <div className='picture'></div>
-            <div className='loginContainer'>
+            <div className='signUpContainer'>
                 <div className='loginContent'>
                     <div className='logoContainer'>
                         <img src={logo} alt="logo"></img>
                     </div>
                     <Formik
                         initialValues={{ email: '', password: '', confirmPassword: '' }}
-                        validate={values => {
-                            const errors = {};
-                            if (!values.email) {
-                                errors.email = 'Required';
-                            } else if (
-                                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                            ) {
-                                errors.email = 'Invalid email address';
-                            }
-                            return errors;
-                        }}
                         onSubmit={async (values, { setSubmitting }) => {
                             setSubmitting(false);
                             await createUserWithEmailAndPassword(auth, values.email, values.password)
@@ -60,41 +72,93 @@ return ( <div className="loginPage">
                             handleSubmit,
                             isSubmitting,
                         }) => (
-                            <form onSubmit={handleSubmit} className="loginForm">
+                            <Form onSubmit={handleSubmit} className="loginForm">
 
-                                <label>Login</label>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    value={values.email}
-                                    className="inputStyle"
-                                />
-                                {errors.email && touched.email && errors.email}
+                                <div className='wrapperNameSurname'>
+                                    <div className='wrapperName'>
+                                        <label>Name</label>
+                                        <input
+                                            type="text"
+                                            name="name"
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.name}
+                                            className='nameInput'
+                                        />
+                                    </div>
+                                    <div className='wrapperSurname'>
+                                        <label>Surname</label>
+                                        <input
+                                            type="text"
+                                            name="surname"
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.surname}
+                                            className='surnameInput'
+                                        />
+                                    </div>
+                                </div>
 
+                                <div className='formFieldContainer'>
+                                    <label>E-mail</label>
+                                    <Field 
+                                        name="email"
+                                        validate={validate}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.email}
+                                        className={`inputStyle ${ errors.email && touched.email && errors.email ? 'inputError' : '' }`}
+                                    />
+                                    <span className='errorMessasge'>{errors.email && touched.email && errors.email}</span>
+                                </div>
 
-                                <label>Password</label>
-                                <input
-                                    type="password"
-                                    name="password"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    value={values.password}
-                                    className="inputStyle"
-                                />
-                                {errors.password && touched.password && errors.password}
+                                <div className='wrapperMaleFamel' role="group" aria-labelledby="my-radio-group">
+                                    <label className='male'>
+                                        <Field type="radio" name="gender" value="Male" />
+                                        Male
+                                    </label>
+                                    <label>
+                                        <Field type="radio" name="gender" value="Female" />
+                                        Female
+                                    </label>
+                                </div>
 
-                                <label>Confirm Password</label>
-                                <input
-                                    type="password"
-                                    name="confirmPassword"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    value={values.confirmPassword}
-                                    className="inputStyle"
-                                />
-                                {errors.confirmPassword && touched.confirmPassword && errors.confirmPassword}
+                                <div className='formFieldContainer'>
+                                    <label>Password</label>
+                                    <div className={`inputStyle ${ errors.password && touched.password && errors.password ? 'inputError' : '' }`} id="passwordDiv">
+                                        <Field
+                                            type={showHidePass ? "text" : "password"}
+                                            validate={passValidate}
+                                            name="password"
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.password}
+                                            className="passworsInput"
+                                        />
+                                        <div className="showHidebtn" onClick={(e) => { setShowHidePass(!showHidePass) }}>
+                                            {showHidePass ? <EyeOutlined /> : <EyeInvisibleOutlined />}
+                                        </div>
+                                    </div>
+                                    <span className='errorMessasge'>{errors.password && touched.password && errors.password}</span>    
+                                </div>
+
+                                <div className='formFieldContainer'>
+                                    <label>Confirm Password</label>
+                                    <div className={`inputStyle ${ errors.password && touched.password && errors.password ? 'inputError' : '' }`} id="passwordDiv">
+                                        <Field
+                                            type={showHideConfirmPass ? "text" : "password"}
+                                            name="confirmPassword"
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.password}
+                                            className="passworsInput"
+                                        />
+                                        <div className="showHidebtn" onClick={(e) => { setShowHideConfirmPass(!showHideConfirmPass) }}>
+                                            {showHideConfirmPass ? <EyeOutlined /> : <EyeInvisibleOutlined />}
+                                        </div>
+                                    </div>
+                                    <span className='errorMessasge'>{errors.password && touched.password && errors.password}</span>    
+                                </div>
 
 
                                 <button type="submit" disabled={isSubmitting} className="loginSubmit">
@@ -104,7 +168,7 @@ return ( <div className="loginPage">
                                 <div>
                                     <span>Already have an account? <Link to={'/signin'}>signin</Link></span>
                                 </div>
-                            </form>
+                            </Form>
                         )}
                     </Formik>
                 </div>
